a7febcee624925a2e8c01426c3bac25abf5cfe94
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-02-11 10:38:55 -0800

    Get MostPopularProduct intent to work

--- index.js
+++ index.js
@@ -26,6 +26,21 @@ var handlers = {
     this.attributes.repromptSpeech = "To find , say something like: what was ?";
     this.emit(":ask", this.attributes.speechOutput, this.attributes.repromptSpeech);
   },
+  "SellItem": function() {
+    console.log("SellItem");
+    var data = {
+      name: 'Premium Quality',
+      type: 'simple',
+      regular_price: '21.99',
+      description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
+      short_description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+    };
+    WooCommerce.post('products', data, function(err, data, res) {
+      var speech = "Product " + data.name + " has been created and is now selling at " + data.regular_price;
+      console.log(speech);
+      _this.emit(":tell", speech);
+    });
+  },
   "GetCustomerOrigins": function() {
     console.log("GetCustomerOrigins");
     _this = this;
@@ -109,9 +124,10 @@ var handlers = {
         return console.log(err);
       }
       var max_product_name;
-      var max_product_sales;
-      product_ids.forEach(function(product) {
-        if (product.total_sales > max_product_sales) {
+      var max_product_sales = 0;
+      var resJSON = JSON.parse(res);
+      resJSON.forEach(function(product) {
+        if (product.total_sales >= max_product_sales) {
           max_product_name = product.name;
           max_product_sales = product.total_sales;
         }
@@ -194,4 +210,4 @@ function getOrderIds(callback) {
   });
 }
 
-handlers.MostReturnedProduct();
+handlers.MostPopularProduct();
